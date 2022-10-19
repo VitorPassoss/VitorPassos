433ba7c4c25dcc396976b0a5d7fe153805dc1865
Vitor Israel Passos Brito <99894543+VitorPassoss@users.noreply.github.com>
2022-10-19 10:31:05 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function modalOne(){
+     let modal = document.querySelector('.modaldiv')
+     modal.showModal()
+}
+
+function modalTwo(){
+    let modal = document.querySelector('.modaldiv1')
+    modal.showModal()
+}
+
+
+
+function exitOne(){
+    let modal = document.querySelector('.modaldiv')
+    modal.close()
+}
+
+function exitTwo(){
+    let modal = document.querySelector('.modaldiv1')
+    modal.close()
+}
+
